d16b21d48feba0b3225833e64894f174fa144e28
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2024-09-26 10:23:09 -0400

    scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="clock">
+      <div className="seconds"></div>
+
+      <div className="container__hours">
+        {/* 5 hours a bar */}
+        <div className="hour-bar"></div>
+        <div className="hour-bar"></div>
+        <div className="hour-bar"></div>
+        <div className="hour-bar"></div>
+        {/* 1 hour a bar */}
+        <div className="hour-bar"></div>
+        <div className="hour-bar"></div>
+        <div className="hour-bar"></div>
+        <div className="hour-bar"></div>
+      </div>
+
+      <div className="container__minutes-5">
+        <div className="minute-bar"></div>
+        <div className="minute-bar"></div>
+        <div className="minute-bar minute-bar--gray"></div>
+        <div className="minute-bar"></div>
+        <div className="minute-bar"></div>
+        <div className="minute-bar minute-bar--gray"></div>
+        <div className="minute-bar"></div>
+        <div className="minute-bar"></div>
+        <div className="minute-bar minute-bar--gray"></div>
+        <div className="minute-bar"></div>
+        <div className="minute-bar"></div>
+      </div>
+
+      <div className="container__minutes-1">
+        {/* 1 minute a bar */}
+        <div className="minute-bar"></div>
+        <div className="minute-bar"></div>
+        <div className="minute-bar"></div>
+        <div className="minute-bar"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
