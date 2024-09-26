914c683687db2ef94d6d9b2b7e855f7ea8f2f914
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2024-09-26 09:57:01 -0400

    setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "modern-normalize/modern-normalize.css";
+import "./styles/styles.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
